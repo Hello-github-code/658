0c01d64dd7fcea10a652c9275b2cae60f7787db3
Hello-github-code <3181425604@qq.com>
2023-12-14 17:40:06 +0800

    DFS更新

--- 658.c
+++ 658.c
@@ -1,5 +1,4 @@
 #include <stdio.h>  
-#include <stdlib.h>  
   
 #define MAXN 100  
 #define MAXM 1000  
@@ -7,19 +6,23 @@
 int n, m;  
 int graph[MAXN][MAXN];  
   
-int dfs(int u, int visited[]) {  
+int dfs(int u, int visited[], int path[], int *path_len) {    
+    visited[u] = 1;  
+    path[*path_len] = u; 
+    (*path_len)++;
+
     if (u == n) {  
         return 1;  
-    }  
-    visited[u] = 1;  
+    } 
+      
     for (int v = 1; v <= n; v++) {  
         if (graph[u][v] && !visited[v]) {  
-            if (dfs(v, visited)) {  
-                return 1;  
-            }  
+            if (dfs(v, visited, path, path_len)){
+                return 1;
+            } 
         }  
     }  
-    return 0;  
+    return 0;
 }  
   
 int main() {  
@@ -29,34 +32,21 @@ int main() {
         scanf("%d %d", &u, &v);  
         graph[u][v] = graph[v][u] = 1;  
     }  
+
     int visited[MAXN];  
     for (int i = 1; i <= n; i++) {  
         visited[i] = 0;  
     }  
-    if (dfs(1, visited)) {  
+
+    int path[MAXN];  
+    int path_len = 0;  
+    if (dfs(1, visited, path, &path_len)) {  
         printf("YES\n");  
-        int path[MAXN];  
-        dfs(1, visited);  
-        path[0] = 1;  
-        int k = 1;  
-        for (int i = 1; i < n; i++) {  
-            if (visited[i]) {  
-                continue;  
-            }  
-            for (int j = 1; j <= n; j++) {  
-                if (graph[i][j] && !visited[j]) {  
-                    path[k++] = j;  
-                    visited[j] = 1;  
-                    break;  
-                }  
-            }  
-        }  
-        for (int i = 0; i < k; i++) {  
+        for (int i = 0; i < path_len; i++) {  
             printf("%d ", path[i]);  
-        }  
-        printf("\n");  
+        }    
     } else {  
-        printf("NO\n");  
+        printf("NO");  
     }  
     return 0;  
 }
